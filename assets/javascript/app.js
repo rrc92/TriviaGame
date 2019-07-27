3aa9c80c1a215a5101b4a0c2cfee86a0a32e2b87
Ricardo Cabrera <ricky_92rc@hotmail.com>
2019-07-27 00:57:37 -0700

    Added form tags and trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,30 @@
-var counter = 30;
-	$('#counter').text(counter);
+    
+    var counter = 120;
+    $('#counter').text(counter);
 
-	var timer = setInterval(goDown, 1*1000);
+    var timer = setInterval(goDown, 1*1000);
 
-	function goDown(){
-		counter--;
-		$('#countdown').text(counter);
+    function goDown(){
+        counter--;
+        $('#countdown').text(counter);
 
-		// if (counter == 0){
-		// 	$("input[type='submit']").attr('disabled', true);
-		// 	clearInterval(timer);
-		// 	$('#content').text('timer is up!')
-		// }
+        if (counter == 0){
+            $("input[type='submit']").attr('disabled', true);
+            clearInterval(timer);
+            $('#content').text('timer is up!')
+        }
 
-	}
+    }
+
+debugger;
+    $("input[type='submit']").on('click', function(){
+        var ans = $("input[name='cap']:checked").val();
+
+        if (ans == 'sacramento'){
+            $('#content').text('correct');
+        }else{
+            $('#content').text('incorrect')
+        }
+
+        event.preventDefault(); //prevent the page from refreshing
+    });
